8f653f4ae3fdf7e66e7147733f975b7a2b176cd4
Farit <faritahmetshin@gmail.com>
2022-11-25 18:23:59 +0300

    findEmptyCell do

--- sudoku.js
+++ sudoku.js
@@ -4,23 +4,45 @@
  * Договорись со своей командой, в каком формате возвращать этот результат.
  */
 function solve(boardString) {
+  function getArr(boardString) {
+    const arr = boardString.split('');
+    let result = [];
+
+    for (let i = 0; i < arr.length; i++) {
+      const breakdown = arr.splice(0, 9);
+      result.push(breakdown);
+    }
+
+    return result;
+  }
+
+  const board = getArr(boardString);
+
+  function findEmptyCell(board) {
+    for (let r = 0; r < board.length; r++) {
+      for (let c = 0; c < board.length; c++) {
+        if (board[r][c] === '-') {
+          return [r, c];
+        }
+      }
+    }
+    return 0;
+  }
 }
 
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
  * Возвращает булевое значение — решено это игровое поле или нет.
  */
-function isSolved(board) {
-
-}
+function isSolved(board) {}
 
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
  * Возвращает строку с игровым полем для последующего вывода в консоль.
  * Подумай, как симпатичнее сформировать эту строку.
  */
-function prettyBoard(board) {
-
+function prettyBoard(sudoku) {
+  console.table(sudoku);
 }
 
 // Экспортировать функции для использования в другом файле (например, readAndSolve.js).
